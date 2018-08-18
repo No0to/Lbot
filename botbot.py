d8164341e56f910d85be0c73986173f683976f95
No0to <leocdean@gmail.com>
2018-08-18 13:01:38 -0700

    'botbot.py'

--- botbot.py
+++ botbot.py
@@ -4,11 +4,13 @@ from colorama import *
 from datetime import timedelta
 from discord.ext.commands import Bot
 from discord import Game
+import discord
+from discord import *
 
 init(convert=True)
 
 print(Fore.GREEN + 'botbot v1.0')
-print(Style.RESET_ALL)
+print(Style.RESET_ALL + '__________')
 
 filename = "token.txt"
 
@@ -53,20 +55,24 @@ async def eight_ball(context):
 
     ]
     await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+    print("$$8ball")
 
 @client.command()
 async def square(number):
     squared_value = int(number) * int(number)
     await client.say(str(number + " squared is " + str(squared_value)))
+    print("$$square")
 
 @client.command()
 async def cube(number):
     cubed_value = int(number) * int(number)
     await client.say(str(number + " cubed is " + str(cubed_value)))
+    print("$$cube")
 
 @client.command()
 async def deadass():
     await client.say("https://imgur.com/a/I0Ktdkc")
+    print("$$deadass")
 
 @client.command()
 async def bitcoin():
@@ -75,34 +81,48 @@ async def bitcoin():
     data = requests.get(bitcoin_price_url).json()
     price_in_usd = data['bpi']['USD']['rate']
     await client.say("The bitcoin price is $" + price_in_usd)
+    print("$$bitcoin")
 
 
 @client.command()
 async def double(number):
     doubled = int(number) * 2
     await client.say(str(number + " doubled is " + str(doubled)))
+    print("$$double")
 
 @client.command()
 async def triple(number):
     tripled = int(number) * 3
     await client.say(str(number + " tripled is " + str(tripled)))
+    print("$$triple")
 
 @client.command()
-async def echo(numbers):
-    await client.say(numbers)
+async def echo(*, words):
+    await client.say(words)
+    print("$$echo")
+
+@client.command(pass_context=True)
+async def getgay(ctx):
+    test = ctx.message.author.mention
+    testt = discord.Member
+    await client.say( str(test) + " Has just came out as gay!")
+    await client.change_nickname(ctx.message.author, ctx.message.author.display_name + '‍🌈')
+    print("$$getgay")
 
 @client.command(pass_context=True)
 async def ping(ctx):
-    await client.say("Pong! ")
+    await client.say("Pong!")
+    print("$$ping")
 
 @client.command(pass_context=True)
 async def about(ctx):
     await client.say("ABOUT```\nMade by thog#6636\n```")
+    print("$$about")
 
 
 @client.event
 async def on_ready():
-    await client.change_status(game=discord.Game(name="Fun Game", url="twitch.tv/streamer", type=1))
+    await client.change_presence(game=discord.Game(name='$$help'))
     print("Logged in as " + client.user.name)
 
 
